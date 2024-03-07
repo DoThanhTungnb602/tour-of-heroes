17e0f0800a785f8577fae610a2ff5a6f752db8fe
hoantr201 <hoantr201@gmail.com>
2024-03-07 18:29:41 +0700

    nothing

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -4,15 +4,27 @@ import { Hero } from '../models/hero';
 import { HeroService } from '../services/hero.service';
 import { RouterLink } from '@angular/router';
 import { HeroSearchComponent } from '../heroes/hero-search/hero-search.component';
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, RouterLink, HeroSearchComponent],
+  imports: [
+    CommonModule,
+    RouterLink,
+    HeroSearchComponent,
+    NzDatePickerModule,
+    FormsModule,
+  ],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
 })
 export class DashboardComponent {
+  date: any;
+  getWeek($event: Event) {
+    console.log($event);
+  }
   heroService: HeroService = inject(HeroService);
   heroes: Hero[] = [];
 
@@ -21,7 +33,8 @@ export class DashboardComponent {
   }
 
   getHeroes(): void {
-    this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes.slice(1, 5));
+    this.heroService
+      .getHeroes()
+      .subscribe((heroes) => (this.heroes = heroes.slice(1, 5)));
   }
 }
